6144c2bab879d39ee8fe46aff20bf92d57a906d2
AbdulelahAlsolami <abodi.said@gmail.com>
2026-05-06 16:16:49 -0400

    Fix Supabase fetch URL

--- js/script.js
+++ js/script.js
@@ -16,8 +16,10 @@ async function searchHero() {
   const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
 
   try {
+    const supabaseUrl = "https://supa-purple.compostela.cloud";
+
     const response = await fetch(
-      `/supabase/rest/v1/characters?select=*&name=ilike.*${heroName}*`,
+      `${supabaseUrl}/rest/v1/characters?select=*&name=ilike.*${heroName}*`,
       {
         headers: {
           apikey: supabaseKey,
